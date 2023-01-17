aad552cfbbaa5b2a0fbeac3d8214243eea10e2f1
Miguel Andres Mosquera <mandresmosquera@gmail.com>
2023-01-17 12:22:56 -0500

    Initi project

---
+++ src/main/java/com/example/TuEnvio/TuEnvioApplication.java
@@ -0,0 +1,13 @@
+package com.example.TuEnvio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuEnvioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuEnvioApplication.class, args);
+	}
+
+}
